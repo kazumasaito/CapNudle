62bf5c5c89e295c4e42812186d02ade351f7d174
12ec057 <hungrykazuma@gmail.com>
2016-11-18 10:26:23 +0900

    プロジェクト作成

---
+++ CapNudleApp/CapNudleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CapNudleApp
+//
+//  Created by 齋藤一真 on 2016/11/18.
+//  Copyright © 2016年 齋藤一真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
