0279473116910f44eb59cfce665d730375d10bb3
kazumasaito <hungrykazuma@gmail.com>
2016-11-22 17:06:41 +0900

    タイマー作成

--- CapNudleApp/CapNudleApp/TimerViewController.swift
+++ CapNudleApp/CapNudleApp/TimerViewController.swift
@@ -14,10 +14,17 @@ class TimerViewController: UIViewController {
     var JANCodeString:String!
     var serverIpAddr:String!
     
+    //時間計測用の変数.
+    var cnt : Float = 180
+    
+    //時間表示用のラベル.
+    var myLabel : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        httpRequest()
+        //httpRequest()
+        self.setTimer()
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,6 +56,8 @@ class TimerViewController: UIViewController {
             
             self.viewText()
             
+            self.setTimer()
+            
         } catch let error as NSError {
             //ユーザーデータが存在しない場合
             print("Failed to load: \(error.localizedDescription)")
@@ -60,5 +69,35 @@ class TimerViewController: UIViewController {
         print("JANコード:\(self.JANCodeString!)")
         print("IPアドレス(クライアント):\(self.ipAddrString!)")
         print("IPアドレス(サーバー):\(self.serverIpAddr!)")
+    }
+    
+    func setTimer() {
+        //ラベルを作る.
+        myLabel = UILabel(frame: CGRect(origin:CGPoint(x:0,y:0), size:CGSize(width:200,height:50)))
+        myLabel.backgroundColor = UIColor.orange
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = 20.0
+        myLabel.text = "Time:\(cnt)"
+        myLabel.textColor = UIColor.white
+        myLabel.shadowColor = UIColor.gray
+        myLabel.textAlignment = NSTextAlignment.center
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        self.view.backgroundColor = UIColor.cyan
+        self.view.addSubview(myLabel)
+        
+        //タイマーを作る.
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(TimerViewController.onUpdate(timer:)), userInfo: nil, repeats: true)
+    }
+    
+    //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
+    func onUpdate(timer : Timer){
+        
+        cnt -= 0.1
+        
+        //桁数を指定して文字列を作る.
+        let str = "Time:".appendingFormat("%.1f",cnt)
+        
+        myLabel.text = str
+        
     }
 }
