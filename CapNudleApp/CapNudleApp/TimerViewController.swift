816f1898f75a79d33d380accc43883b8c319fe80
kazumasaito <hungrykazuma@gmail.com>
2016-11-22 16:29:41 +0900

    API通信実装（仮）

--- CapNudleApp/CapNudleApp/TimerViewController.swift
+++ CapNudleApp/CapNudleApp/TimerViewController.swift
@@ -12,10 +12,12 @@ import UIKit
 class TimerViewController: UIViewController {
     var ipAddrString:String!
     var JANCodeString:String!
+    var serverIpAddr:String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewText()
+        
+        httpRequest()
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,8 +25,40 @@ class TimerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func httpRequest() {
+        let url = URL(string:"http://27.120.120.174/NoodleApp/Test.php")
+        
+        let task = URLSession.shared.dataTask(with: url!){ data, response, error in
+            OperationQueue.main.addOperation {
+                self.parseJson(data: data, error: error)
+            }
+        }
+        task.resume()
+    }
+    
+    /**
+     * レスポンスデータをパースする
+     */
+    func parseJson(data: Data?, error: Error?) {
+        do {
+            let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            
+            print(parsedData)
+            
+            self.serverIpAddr = parsedData["ip_addr"] as! String!
+            
+            self.viewText()
+            
+        } catch let error as NSError {
+            //ユーザーデータが存在しない場合
+            print("Failed to load: \(error.localizedDescription)")
+        }
+        
+    }
+    
     func viewText() {
         print("JANコード:\(self.JANCodeString!)")
-        print("IPアドレス:\(self.ipAddrString!)")
+        print("IPアドレス(クライアント):\(self.ipAddrString!)")
+        print("IPアドレス(サーバー):\(self.serverIpAddr!)")
     }
 }
